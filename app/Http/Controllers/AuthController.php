321226d1753f67c1a5f60628fe841e4cf8e9e5d1
lakroune <lakroune00@gmail.com>
2026-03-09 13:43:16 +0000

    Implement user registration in AuthController with token generation and update User model to include role and token fields.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
-public function  register(RegisterRequest $request){
-    return $request->all();
+
+    public function  register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::create($data);
+        $token = $user->createToken('main')->plainTextToken;
+
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
 }
---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|string|email|exists:users,email|max:255',
+            'password' => 'required|string|min:8',
+        ];
+    }
+    public function messages()
+    {
+        return   [
+            'email.required' => 'L\'email est requis',
+            'password.required' => 'Le mot de passe est requis',
+            'email.exists' => 'L\'email n\'existe pas',
+            'password.min' => 'Le mot de passe doit avoir au moins 8 caractères',
+            'email.email' => 'L\'email n\'est pas valide',
+        ];
+    }
+}
