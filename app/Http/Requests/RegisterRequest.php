f812b14c1cb7369d0f9bb38974fe17b3ee2ad445
lakroune <lakroune00@gmail.com>
2026-03-09 14:28:59 +0000

    Update authorization in LoginRequest and RegisterRequest, modify welcome view for new layout, and enhance web routes documentation

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -11,7 +11,7 @@ class RegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -26,7 +26,6 @@ class RegisterRequest extends FormRequest
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
             'password_confirmation' => 'required|string|min:8',
-            'role' => 'required|in:admin,user',
         ];
     }
 
