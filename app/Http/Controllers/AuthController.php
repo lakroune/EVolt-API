c874042ea9c85f0f98970652888ccb37f9741b74
lakroune <lakroune00@gmail.com>
2026-03-09 13:01:05 +0000

    Integrate Laravel Sanctum into User model for API token management.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    
+public function  register(RegisterRequest $request){
+    return $request->all();
+}
---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'password_confirmation' => 'required|string|min:8',
+            'role' => 'required|in:admin,user',
+        ];
+    }
+
+    public function messages()
+    {
+        return   [
+            'name.required' => 'Le nom est requis',
+            'email.required' => 'L\'email est requis',
+            'password.required' => 'Le mot de passe est requis',
+            'password_confirmation.required' => 'La confirmation du mot de passe est requise',
+            'role.required' => 'Le rôle est requis',
+        ];
+    }
+}
